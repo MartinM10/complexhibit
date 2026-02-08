f60fb16428e6c665316eece230de44010c77b6e3
MartinM10 <martin.salvachua1@gmail.com>
2026-02-08 01:08:53 +0100

    style(frontend): configure Geist font and update global styles

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,9 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
         <Navbar />
         <main className="flex-grow bg-gray-50">
           {children}
